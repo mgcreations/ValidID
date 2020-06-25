371040c2fa43297d47d93f755c942a81f1135901
MG Creations <info@mgcreations.co.za>
2020-06-25 05:26:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ValidID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Console.Write("Enter an ID Numer: ");
+            input = Console.ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while(idNum < LOW || idNum > HIGH)
+            {
+                Console.WriteLine("{0} is an invalid ID Number", idNum);
+                Console.Write("Id number must be ");
+                Console.WriteLine("between {0} and {1} inclusive", LOW, HIGH);
+
+                Console.Write("Enter an ID Number: ");
+                input = Console.ReadLine();
+                idNum = Convert.ToInt32(input);
+            }
+
+            Console.WriteLine("ID Number {0} is valid", idNum);
+        }
+    }
+}
